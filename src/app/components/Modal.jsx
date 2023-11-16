18e553385217ed80d811bac6499778a048a3ff55
thefordz <thefordz.th@gmail.com>
2023-11-16 19:34:05 +0700

    add height, width for setting size of modal

--- src/app/components/Modal.jsx
+++ src/app/components/Modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 
-const Modal = ({toggleModal, children}) => {
+const Modal = ({toggleModal, children, height ,width}) => {
 
     const opacityVariants = {
         initial : {opacity : 0},
@@ -18,13 +18,13 @@ const Modal = ({toggleModal, children}) => {
 
   return (
     // overlay
-    <motion.div variants={opacityVariants} initial="initial" animate="animate" exit="exit" className=' fixed w-screen h-screen bg-black/50 backdrop-blur-sm'>
+    <motion.div variants={opacityVariants} initial="initial" animate="animate" exit="exit" className=' fixed w-screen h-screen bg-black/50 backdrop-blur-sm flex items-center justify-center'>
         {/* wrapper */}
-        <div className=" relative container mx-auto p-8 w-full h-full">
-            {/* close button */}
-            <motion.button whileHover={{scale : 1.1}} whileTap={{scale : 0.9}} type='button' onClick={toggleModal} className='z-10 w-10 h-10 top-10 right-10 bg-black text-white rounded-full flex items-center justify-center absolute'>X</motion.button>
+        <div className="w-full h-full flex items-center justify-center container mx-auto p-8">
             {/* modal */}
-            <motion.div variants={opacityVariants} initial="initial" animate="animate" exit="exit" transition={{duration : .3, ease : [0.26, 1, 0.32, 1]}} className="bg-white w-full h-full px-5 rounded-3xl">
+            <motion.div variants={opacityVariants} initial="initial" animate="animate" exit="exit" transition={{duration : .3, ease : [0.26, 1, 0.32, 1]}} className={`w-[${width}] h-[${height}] bg-white relative mx-auto my-auto px-5 rounded-3xl`}>
+                {/* close button */}
+                <motion.button whileHover={{scale : 1.1}} whileTap={{scale : 0.9}} type='button' onClick={toggleModal} className='z-10 w-10 h-10 top-2 right-2 bg-black text-white rounded-full flex items-center justify-center absolute'>X</motion.button>
                 {/* content */}
                 <motion.div variants={contentVariants} initial="initial" animate="animate" exit="exit" className=" w-full h-full flex flex-col overflow-hidden overflow-y-scroll scrollbar-hide">
                     {children}
